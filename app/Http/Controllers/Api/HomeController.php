25014445dfaad04670600df86e666bd358eef11f
ahmad <ahmadalshuga8@gmail.com>
2025-05-02 13:02:06 +0300

    fix collection

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -6,12 +6,15 @@ use App\{Enums\StatusEnum,
     Http\Controllers\Controller,
     Http\Resources\Catalog\BrandResource,
     Http\Resources\Catalog\CategoryResource,
+    Http\Resources\Catalog\CollectionResource,
     Http\Resources\Catalog\ProductResource,
     Pipelines\Catalog\BrandFilterPipeline,
     Pipelines\Catalog\CategoryFilterPipeline,
     Pipelines\Settings\PostFilterPipeline,
+    Pipelines\SortFilterPipeline,
     Services\Catalog\BrandService,
     Services\Catalog\CategoryService,
+    Services\Catalog\CollectionService,
     Services\Catalog\ProductService,
     Services\Settings\PostService,
     Utils\HttpFoundation\Response};
@@ -45,6 +48,12 @@ class HomeController extends Controller
             data: [
                 'flash_sale' => ProductResource::collection(app(ProductService::class)->getAll(filters: [], relations:['images','userWishlist','variants.attributeOptions.attribute'], limit: 4)),
                 'top_rated' => ProductResource::collection(app(ProductService::class)->getAll(filters: [], relations:['images','userWishlist','variants.attributeOptions.attribute'], limit: 4)),
+                'top_collections' => CollectionResource::collection(app(CollectionService::class)->getAll(filters: [
+                    new SortFilterPipeline(sortByColumn: 'id', sortType: 'desc')
+                ],limit: 3)),
+                'bottom_collections' => CollectionResource::collection(app(CollectionService::class)->getAll(filters: [
+                    new SortFilterPipeline(sortByColumn: 'id', sortType: 'desc')
+                ],limit: 9)),
             ]
         );
     }
